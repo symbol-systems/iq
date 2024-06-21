482cbd5ea085d5e4346871e506981863e91f69e4
Lee <lee@troven.co>
2024-06-21 23:22:33 +1000

    clean

--- iq-trusted/my.iq/import/v0/I/research.groovy
+++
@@ -1,8 +0,0 @@
-import systems.symbol.agent.IQFacade
-
-if (!my.prompt) return;
-iq = (IQFacade)iq;
-my = (Map<String,String>)my;
-
-def results = iq.api("https://api.search.brave.com/res/v1/web/search","x-subscription-token").get([q: my.prompt ]);
-my.results = iq.json(results.body());
