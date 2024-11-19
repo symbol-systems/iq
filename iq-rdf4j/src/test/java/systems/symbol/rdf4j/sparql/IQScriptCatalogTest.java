dd99cda5098156f1acc0593d794f94783a30a0a4
Lee <lee@troven.co>
2024-11-19 18:08:12 +1100

    test fix 3

--- iq-rdf4j/src/test/java/systems/symbol/rdf4j/sparql/IQScriptCatalogTest.java
+++ iq-rdf4j/src/test/java/systems/symbol/rdf4j/sparql/IQScriptCatalogTest.java
@@ -1,17 +1,44 @@
 package systems.symbol.rdf4j.sparql;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.rio.RDFFormat;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import systems.symbol.COMMONS;
 import systems.symbol.platform.IQ_NS;
 import systems.symbol.rdf4j.io.RDFDump;
-import systems.symbol.rdf4j.store.AbstractTripleTest;
+import systems.symbol.rdf4j.store.BootstrapRepository;
 import systems.symbol.rdf4j.store.IQConnection;
 import systems.symbol.rdf4j.store.LiveModel;
 
-public class IQScriptCatalogTest extends AbstractTripleTest {
+public class IQScriptCatalogTest {
+public BootstrapRepository assets;
+public ValueFactory vf;
+public IRI ctx, iriSparqlQuery, iriTestCase;
+public IRI iriHBSTemplate;
+
+@BeforeAll
+public void setUp() throws IOException {
+assets = new BootstrapRepository();
+ctx = assets.load(new File("src/test/resources/assets"), IQ_NS.TEST);
+vf = assets.getValueFactory();
+
+iriTestCase = vf.createIRI(IQ_NS.TEST + "TestCase");
+iriSparqlQuery = vf.createIRI("iq:test:queries/all");
+iriHBSTemplate = vf.createIRI("iq:test:hbs/index");
+}
+
+@AfterAll
+public void tearDown() {
+assets.shutDown();
+}
 
 @Test
 public void testGetSPARQLWithIRIFromConnection() {
