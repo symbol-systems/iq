6950292b99d4c0b9ec6f65292bdc5b6fe9861857
JO <JO@symbol.systems>
2026-03-20 18:23:54 +1100

    secrets management

---
+++ iq-connect/iq-connect-github/src/main/java/systems/symbol/connect/github/GithubMyselfScanner.java
@@ -0,0 +1,28 @@
+package systems.symbol.connect.github;
+
+import java.io.IOException;
+
+import org.eclipse.rdf4j.model.IRI;
+
+import org.kohsuke.github.GHMyself;
+import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GHRepository;
+
+final class GithubMyselfScanner {
+
+private GithubMyselfScanner() {
+}
+
+static void scan(GHMyself me, GithubScanContext context) throws IOException {
+IRI userIri = context.modeller().rootUser(context.connectorId(), me.getLogin(), me.getName());
+
+for (GHRepository repo : me.listRepositories()) {
+GithubRepositoryScanner.scan(repo, userIri, context);
+}
+
+for (GHOrganization organization : me.getAllOrganizations()) {
+IRI orgIri = context.modeller().organization(organization.getLogin(), organization.getName());
+context.modeller().linkResource(userIri, orgIri);
+}
+}
+}
